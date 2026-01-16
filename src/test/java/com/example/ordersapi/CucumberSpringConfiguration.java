5decad8695edc6107284e039fdef72394dcadeb9
Fictor86 <yoypicky@hotmail.com>
2026-01-16 12:49:53 +0100

    Integrate Testcontainers for Cucumber tests and configure PostgreSQL

--- src/test/java/com/example/ordersapi/CucumberSpringConfiguration.java
+++ src/test/java/com/example/ordersapi/CucumberSpringConfiguration.java
@@ -2,8 +2,12 @@ package com.example.ordersapi;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainersConfig.class)
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
 }
